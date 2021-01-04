68ab2663605b9225d0869a7f763eeca1a1f53d9a
dianalynshull <dianalynshull@gmail.com>
2021-01-04 12:11:48 -0700

    added the install event listener to the serviceworker

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,3 +8,17 @@ const filesToCache = [
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png'
 ];
+
+self.addEventListener('install', (e) => {
+    console.log('hit install');
+
+    e.waitUntil(
+        caches
+            .open(fileCacheName)
+            .then(cache => {
+                return cache.addAll(filesToCache);
+            })
+            .catch(err => console.log('Error caching files on install: ', err))
+    );
+    self.skipWaiting();
+});
