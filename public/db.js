8bb859bddcba50a3d88025f8008eb4d8aa6814da
dianalynshull <dianalynshull@gmail.com>
2021-01-04 13:09:13 -0700

    added onsuccess and onerror event listeners for database connection

--- public/db.js
+++ public/db.js
@@ -6,3 +6,14 @@ request.onupgradeneeded = e => {
     const db = e.target.result;
     db.createObjectStore('pending', { autoIncrement: true });
 };
+
+request.onsucces = e => {
+    db = e.target.result;
+    if (navigator.onLine) {
+        // add function that will check all pending transactions and send them through once online
+    }
+};
+
+request.onerror = e => {
+    console.log('Error on connection: ', e.target.errorCode);
+}
