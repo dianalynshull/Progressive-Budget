2e5f7bca2ab8d7bd0e94b07ec89d1302dd16d44c
dianalynshull <dianalynshull@gmail.com>
2021-01-04 12:05:59 -0700

    added the fileCacheName  and the filesToCache to the service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,10 @@
-console.log('reached service worker file');
+const fileCacheName = 'budget-v1';
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/index.js',
+    '/styles.css',
+    '/manifest.webmanifest',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
